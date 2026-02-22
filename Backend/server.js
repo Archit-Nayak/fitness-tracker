c026eb00cc9404e80cd5c719fc3b7fba775dfe61
Archit Abhijit Nayak <architabhijitnayak@gmail.com>
2026-02-22 11:48:41 +0530

    Fixed CORS for production

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,23 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://fitness-tracker-wheat-one.vercel.app"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      return callback(new Error("CORS not allowed"), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
